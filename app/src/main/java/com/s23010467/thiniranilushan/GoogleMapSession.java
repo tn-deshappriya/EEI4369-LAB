b35d16def6aed8e27e5cabcef0647da231dcd695
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-06-15 23:07:19 +0530

    First commit

---
+++ app/src/main/java/com/s23010467/thiniranilushan/GoogleMapSession.java
@@ -0,0 +1,46 @@
+package com.s23010467.thiniranilushan;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.ICameraUpdateFactoryDelegate;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GoogleMapSession extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_google_map_session);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        SupportMapFragment mapFragment =(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+    myMap = googleMap;
+    LatLng myplace= new LatLng(6.883114420333968, 79.88668496664357);
+    myMap.addMarker(new MarkerOptions().position(myplace).title("Marker in OUSL"));
+    myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(18.0f));
+    myMap.getUiSettings().setZoomControlsEnabled(true) ;
+    }
+}
